6334d3f1ad79689c3046d74f79e6055c6b0f7fc8
brunocgf <brunocgf@gmail.com>
2020-05-17 10:47:24 -0500

    documentacion solver

--- notebooks/Programacion/solver/extraer_datos_yahoo.py
+++ notebooks/Programacion/solver/extraer_datos_yahoo.py
@@ -1,10 +1,16 @@
 import fix_yahoo_finance as yf
 
-def extraer_datos_yahoo(stocks):
+def extraer_datos_yahoo(stocks, start='2015-01-01', end='2020-04-30'):
   '''
-  Funcion para extraer datos de los portafolios de yahoo finance de 2015-01-01 a 2020-04-30
+  Funcion para extraer precios al cierre de las acciones mediante yahoo finance de 2015-01-01 a 2020-04-30
+
+  params: stocks    lista de acciones de las cuales se desea obtener el precio
+          start     fecha inicial
+          end       fecha final
+
+  return: base      Dataframe de precios por acción (columnas) y día (filas)
   '''
-  df_c = yf.download(stocks, start='2015-01-01', end='2020-04-30').Close
+  df_c = yf.download(stocks, start=start, end=end).Close
   base = df_c['AAPL'].dropna().to_frame()
   for i in range(0,50):
       base = base.join(df_c.iloc[:,i].to_frame(), lsuffix='_caller', rsuffix='_other')
